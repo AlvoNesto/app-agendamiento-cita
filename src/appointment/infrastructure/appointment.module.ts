eefa7f56e8c78a9c539011dad7253bb49cf0903f
Alvaro <anarvaez@synopsis.onmicrosoft.com>
2025-09-08 08:21:45 -0500

    new deploy

--- src/appointment/infrastructure/appointment.module.ts
+++ src/appointment/infrastructure/appointment.module.ts
@@ -3,20 +3,13 @@ import { AppointmentController } from './appointment.controller';
 import { AppointmentService } from '../application/appointment.service';
 import { DynamoDBAppointmentRepository } from './dynamodb-appointment.repository';
 import { SnsRecievedPublisher } from './sns-recieved.publisher';
-import { AppointmentRepository } from '../domain/appointment.repository';
-import { RecievedPublisher } from '../domain/recieved.publisher';
 
 @Module({
   controllers: [AppointmentController],
   providers: [
-    { provide: 'AppointmentRepository', useClass: DynamoDBAppointmentRepository },
-    { provide: 'EventPublisher', useClass: SnsRecievedPublisher },
-    {
-      provide: AppointmentService,
-      useFactory: (repo: AppointmentRepository, publisher: RecievedPublisher) =>
-        new AppointmentService(repo as any, publisher as any),
-      inject: ['AppointmentRepository','EventPublisher']
-    }
+    DynamoDBAppointmentRepository,
+    SnsRecievedPublisher,
+    AppointmentService,
   ],
 })
 export class AppointmentModule {}
